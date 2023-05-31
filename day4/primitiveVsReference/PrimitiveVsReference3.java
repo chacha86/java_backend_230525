76293bf5a94e98c521c644246ac051e2f89f2e09
chacha86 <taejincha0619@gmail.com>
2023-05-31 11:49:11 +0900

    자바 지역변수, 참조타입과 메모리구조

---
+++ day4/primitiveVsReference/PrimitiveVsReference3.java
@@ -0,0 +1,10 @@
+package day4.primitiveVsReference;
+
+public class PrimitiveVsReference3 {
+    public static void main(String[] args) {
+
+        // 배열은 객체, arr은 주소값이다.
+        int[] arr = new int[5];
+
+    }
+}
