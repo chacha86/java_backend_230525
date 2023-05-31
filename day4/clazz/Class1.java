dc63d1d44485e4c292b8eb3d7bc29a808d7a094f
chacha86 <taejincha0619@gmail.com>
2023-05-31 12:49:08 +0900

    class의 메서드

--- day4/clazz/Class1.java
+++ day4/clazz/Class1.java
@@ -4,7 +4,9 @@ package day4.clazz;
 public class Class1 {
 
     // 이름 짓는 법 -> 카멜표기법, 파스칼 표기법
-
+    public static void introduce(String name, String address, double height) {
+        System.out.printf("안녕하세요 키 %f cm인 %s입니다. %s 살고 있어요.\n", height, name, address);
+    }
     // 메서드, 변수 -> 카멜표기법
     public static void printPersonInfo(int age, int height, int weight) {
         System.out.println(age);
@@ -13,7 +15,7 @@ public class Class1 {
     }
 
     public static void printPersonInfo2(int[] person) {
-        System.out.println(person[0]);
+     System.out.println(person[0]);
         System.out.println(person[1]);
         System.out.println(person[2]);
     }
@@ -63,30 +65,49 @@ public class Class1 {
         arr2[1] = 2;
 
         // 클래스를 이용해 객체 만들기
+        // new Person()은 클래스를 본따서 만드는 복사본이고 hong1은 그걸 가리키는 참조변수이다.
         Person hong1 = new Person();
-
         hong1.name = "홍길동";
         hong1.address = "서울";
         hong1.height = 176.9;
+
+        // new Person()은 클래스를 본따서 만드는 복사본이고 lee는 그걸 가리키는 참조변수이다.
+        Person lee = new Person();
+        lee.name = "이순신";
+        lee.address = "대전";
+        lee.height = 170.1;
+
 
         System.out.println(hong1.name);
         System.out.println(hong1.address);
         System.out.println(hong1.height);
 
+        // 함수 호출은 같은 class 내의 함수 호출 가능.
+        introduce(hong1.name, hong1.address, hong1.height);
+        introduce(lee.name, lee.address, lee.height);
 
-        // 개발자2가 생각한 사람
-//        String name4 = "홍길동";
-//        int age4 = 20;
-//        double height4 = 176.9;
-
-
-
-
+        // 함수를 타클래스 내에 만드는 이유는 타클래스 내의 데이터를 사용하게 하기 위함이다.
+        hong1.introduce();
+        lee.introduce();
+        
     }
 }
 
+// class 여러 유형의 데이터와 그 데이터를 사용하는 메서드의 집합
 class Person {
     String name; // 사람 이름
     String address; // 사람 주소
     double height; // 사람 키
+
+    int age;
+
+    // introduce함수의 height, name, address는 Person 클래스 내에 이미 존재하므로 매개변수로 넘길 필요가 없다.
+    public void introduce() {
+        System.out.printf("안녕하세요 키 %f cm인 %s입니다. %s 살고 있어요.\n", height, name, address);
+    }
+    public void addAge() {
+        age++;
+    }
+
+
 }
