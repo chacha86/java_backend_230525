3ed97315e03881743e3918c12ff10d43d2295c00
chacha86 <taejincha0619@gmail.com>
2023-06-01 12:02:25 +0900

    생성자 개념 수정본

--- day5/constructor/Constructor1.java
+++ day5/constructor/Constructor1.java
@@ -3,24 +3,25 @@ package day5.constructor;
 public class Constructor1 {
     public static void main(String[] args) {
 
-        int a; // 선언
-        a = 10; // 초기화
-
         // 객체를 만들면 반드시!!!!!!!!!! 데이터를 넣어줘야 한다.(초기화)
-        Person p1 = new Person(1, 20, "홍길동");
-        // 객체 초기화
-        //        // 1. 객체 초기화 불편. 귀찮.
-        //        // 2. 초기화를 까먹는다.
-        //
-        //        // 저 위에 두개 해결하려고 생성자를 사용한다.
-//        p1.no = 1;
-//        p1.age = 20;
-//        p1.name = "홍길동";
 
-//        Person p2 = new Person();
+        //객체 초기화
+        // 1. 객체 초기화 불편. 귀찮.
+        // 2. 초기화를 까먹는다.
+
+        // 저 위에 두개 해결하려고 생성자를 사용한다.
+        // 아래 방식은 불편하고 초기화 과정이 누락될 수 있음.
+        // Person p1 = new Person();
+        // p1.no = 1;
+        // p1.age = 20;
+        // p1.name = "홍길동";
+
+        // 아래처럼 생성자를 이용한 초기화 방식은 간편하고 누락될 염려가 없음.
+        Person p1 = new Person(1, 20, "홍길동");
+        Person p2 = new Person(2, 30, "홍길순");
 
         p1.introduce();
-//        p2.introduce();
+        p2.introduce();
 
 
     }
@@ -36,14 +37,14 @@ class Person {
     // 2. 메서드이름을 꼭! 클래스명과 동일하게 지어야 함.
     // 3. 이 메서드는 반드시 실행됨.
 
-    public Person(int no, int age, String name) {
+    public Person(int a, int b, String c) {
         System.out.println("사람이 한명 생성됨.");
-        System.out.println("번호 : " + no);
-        System.out.println("나이 : " + age);
-        System.out.println("이름 : " + name);
-        no = no;
-        age = age;
-        name = name;
+        System.out.println("번호 : " + a);
+        System.out.println("나이 : " + b);
+        System.out.println("이름 : " + c);
+        no = a;
+        age = b;
+        name = c;
     }
 
     // 처리
