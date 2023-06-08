68c1f7083dfd2a6937ccbe63c318f075153190b5
chacha86 <taejincha0619@gmail.com>
2023-06-08 11:58:55 +0900

    인터페이스 다시

--- day8/interface1/Interface1.java
+++ day8/interface1/Interface1.java
@@ -1,2 +1,49 @@
-package day8.interface1;public class Interface1 {
+package day8.interface1;
+
+public class Interface1 {
+    public static void main(String[] args) {
+
+        사람 a사람 = new 사람();
+
+        요리사 a요리사 = a사람;
+        a요리사.조리();
+
+        아버지 a아버지 = a사람;
+        a아버지.애들케어();
+
+
+    }
+}
+class 전사 {
+    무기 a무기;
+    void 공격() {
+        a무기.사용();
+    }
+}
+
+// 구현과 형식을 분리(형식만)
+interface 무기 {
+    // 추상메서드 -> 내용 없는 메서드(반드시 오버라이딩 해야됨)
+    void 사용();
+    void 무기정보();
+    void 수리();
+}
+
+class 사람 implements 요리사, 아버지 {
+    public void 조리() {
+        System.out.println("5성급 호텔 요리 실력으로 조리합니다.");
+    }
+
+    public void 애들케어() {
+        System.out.println("주말마다 애들과 놀아줍니다.");
+    }
+
+}
+
+interface 요리사 {
+    void 조리();
+}
+
+interface 아버지 {
+    void 애들케어();
 }
